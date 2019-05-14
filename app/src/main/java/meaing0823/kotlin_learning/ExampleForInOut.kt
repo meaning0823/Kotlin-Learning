45db48b5c7a334aef1f1ad1ae30ec6de56006894
meaning0823 <meaning0823@gmail.com>
2019-05-14 17:01:49 +0800

    Update ExampleForInOut.kt

--- app/src/main/java/meaing0823/kotlin_learning/ExampleForInOut.kt
+++ app/src/main/java/meaing0823/kotlin_learning/ExampleForInOut.kt
@@ -18,15 +18,24 @@ class ExampleForInOut {
         }
     }
 
-    fun method01() {
-        val pool1 = Pool<OneDollar>()
-        pool1.add(OneDollar())
+    /**
+     * This function is used to explain why <in > will only return Any?
+     */
+    fun function01() {
+        val pool0 = Pool<Any>()
+        pool0.add("nothing")
+
+        val pool1: Pool<in OneDollar> = pool0
+        pool1.add(OneDollar()) // ["nothing", OneDollar]
 
         val pool2: Pool<in TwoDollar> = pool1
-        pool2.add(TwoDollar())
+        pool2.add(TwoDollar()) // ["nothing", OneDollar, TwoDollar]
 
         val pool3: Pool<in ThreeDollar> = pool2
-        pool3.add(ThreeDollar())
-    }
+        pool3.add(ThreeDollar()) // ["nothing", OneDollar, TwoDollar, ThreeDollar]
 
+        val value1: Any? = pool1.get() // You only can expect it return Any?
+        val value2: Any? = pool2.get()
+        val value3: Any? = pool3.get()
+    }
 }
